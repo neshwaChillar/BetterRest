e761c0ca8859f6aa4c4833302205e65f7aa9e924
neshwa.parveen <neshwa.parveen@chillarcards.com>
2023-12-14 15:57:50 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Neshwa on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
